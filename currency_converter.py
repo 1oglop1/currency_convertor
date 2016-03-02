4d1f0a687bae3acd0f59de15cbc6cf14fad9594c
oglop <Jan@winPC>
2016-03-02 08:48:40 +0100

    added 2 new functions: non_eur_convert from_eur_convert
    removed one function
    convert_amount
    
    updated readme.md

--- currency_converter.py
+++ currency_converter.py
@@ -15,9 +15,7 @@ def update_local_rates():
     return tree
 
 
-def convert_amount(amount, in_curr, currency_root, out_curr):
-    path = "{{http://www.ecb.int/vocabulary/2002-08-01/eurofxref}}Cube/.[@currency='{c}']"
-
+def non_eur_convert(amount, in_curr, currency_root, out_curr, path):
     in_curr = currency_root.find(path.format(c=in_curr)).attrib
     in_curr_rate = float(in_curr['rate'])
     in_curr_currency = in_curr['currency']
@@ -40,19 +38,41 @@ def convert_amount(amount, in_curr, currency_root, out_curr):
             else:
                 print('Cannot convert from {fr} to {to}'.format(fr=in_curr_currency, to=oc), file=sys.stderr)
             continue
-# a
+
+
+def from_eur_convert(amount, in_curr, currency_root, out_curr, path):
+    for oc in out_curr:
+        try:
+            tmp_out_curr = currency_root.find(path.format(c=oc)).attrib
+            #  print(tmp_out_curr)
+            oc_rate = float(tmp_out_curr['rate'])
+            oc_currency = tmp_out_curr['currency']
+            wanted = oc_rate * amount
+            # print("{am} {fr} = {res} {to}".format(am=amount, fr=in_curr_currency, res=wanted, to=oc_currency))
+            yield oc_currency, wanted
+        except (KeyError, AttributeError):
+            if oc == 'EUR':
+                # print("{am} {fr} = {res} {to}".format(am=amount, fr=in_curr_currency, res=wanted, to=oc))
+                yield in_curr, amount
+            else:
+                print('Cannot convert from {fr} to {to}'.format(fr=in_curr, to=oc), file=sys.stderr)
+            continue
+
+
 def cmd_args():
     parser = argparse.ArgumentParser(description='Currency Converter, source file fom ECB')
     parser.add_argument('amount', metavar='<amount>', type=float,
-                       help='Amount to be converted')
+                        help='Amount to be converted')
     parser.add_argument('fr', metavar='<from>',
-                       help='Input currency')
+                        help='Input currency')
 
     parser.add_argument('to', metavar='<to>', nargs='*',
-                        default= ['EUR', 'USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'NOK',
-                                  'HRK', 'RUB', 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MXN',
-                                  'MYR', 'NZD', 'PHP', 'SGD', 'THB', 'ZAR'],
-                       help='Output currency(ies), EUR, CZK...')
+                        default=['EUR', 'USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF',
+                                 'NOK',
+                                 'HRK', 'RUB', 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW',
+                                 'MXN',
+                                 'MYR', 'NZD', 'PHP', 'SGD', 'THB', 'ZAR'],
+                        help='Output currency(ies), EUR, CZK...')
 
     parser.add_argument('-json',
                         action='store_true',
@@ -63,7 +83,6 @@ def cmd_args():
 
 
 def main():
-
     args = cmd_args()
     args.to = list(map(lambda x: x.strip(',;'), args.to))
 
@@ -78,17 +97,26 @@ def main():
 
     now = dt.datetime.now()
 
-    if (currency_time != now.date().isoformat()) and now.hour > 16:  # updated today after 16:00
+    # update local XML in case of old info
+    if (currency_time != now.date().isoformat()) and now.hour > 16 and now.weekday() not in (5, 6):
+        print(currency_time)
         tree = update_local_rates()
         root = tree.getroot()
 
     cube = root[2][0]
 
-    converted_dict = dict((k, v) for (k, v) in convert_amount(10, args.fr , cube, args.to))
+    path = "{{http://www.ecb.int/vocabulary/2002-08-01/eurofxref}}Cube/.[@currency='{c}']"
 
-    output = { "input": { "amount": args.amount, "currency": args.fr},
-          "output": converted_dict
-    }
+    if args.fr == 'EUR':
+        res_gen = from_eur_convert(args.amount, args.fr, cube, args.to, path)
+    else:
+        res_gen = non_eur_convert(args.amount, args.fr, cube, args.to, path)
+
+    converted_dict = dict((k, v) for (k, v) in res_gen)
+
+    output = {"input": {"amount": args.amount, "currency": args.fr},
+              "output": converted_dict
+              }
 
     if args.json:
         with open('currency.json', 'w') as oj:
@@ -96,7 +124,6 @@ def main():
                 oj.write(chunk)
     else:
         print(json.dumps(output, sort_keys=True, indent=4))
-
 
 
 if __name__ == '__main__':
